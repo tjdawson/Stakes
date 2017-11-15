e85c142ee3b95ec63771b3916abe0dc1ea6033e0
tij <tim.joeldawson@gmail.com>
2017-11-14 21:27:30 -0500

    Checkbox
    ...

--- js_includes/Form_CheckboxValidate.js
+++ js_includes/Form_CheckboxValidate.js
@@ -82,17 +82,29 @@ jqueryWidget: {
                 }
 
                 var checks = $(dom).find("input[type=checkbox]");
+                var any_checked = false;
+                if(checks){
                 for (var i = 0; i < checks.length; ++i) {
                     var check = $(checks[i]);
  
                     // Checkboxes with the 'obligatory' class must be checked.
-                    if (! check.attr('checked') && check.hasClass('obligatory')) {
-                        alertOrAddError(check.attr('name'), t.obligatoryCheckboxErrorGenerator(check.attr('name')));
-                        return;
+                    // if (! check.attr('checked') && check.hasClass('obligatory')) {
+                    //     alertOrAddError(check.attr('name'), t.obligatoryCheckboxErrorGenerator(check.attr('name')));
+                    //     return;
+                    // }
+                    if (check.attr('checked')) {
+                        any_checked = true;
                     }
+
 
                     rlines.push([["Field name", check.attr('name')],
                                  ["Field value", check.attr('checked') ? t.checkedValue : t.uncheckedValue]]);
+                }
+
+                if (!any_checked) {
+                    alertOrAddError(check.attr('name'), 'You must check at least one checkbox to continue.');
+                    return;
+                }
                 }
 
                 var rads = $(dom).find("input[type=radio]");
